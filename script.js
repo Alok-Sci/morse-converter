ef11f2f8776a76746aabc7ec4a57f2f1a6ef8350
Alok-Sci <alokscientist7905@gmail.com>
2024-02-20 13:25:47 +0530

    refactor: web audio api approach

--- script.js
+++ script.js
@@ -34,8 +34,31 @@ const morse = {
     '6': '-....',
     '7': '--...',
     '8': '---..',
-    '9': '----.'
+    '9': '----.',
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "'": ".----.",
+    "/": "-..-.",
+    "(": "-.--.",
+    ")": "-.--.-",
+    "&": ".-...",
+    ":": "---...",
+    ";": "-.-.-.",
+    "=": "-...-",
+    "+": ".-.-.",
+    "-": "-....-",
+    "_": "..--.-",
+    "\"": ".-..-.",
+    "$": "...-..-",
+    "!": "-.-.--",
+    "@": ".--.-."
 };
+
+const morseAudio = {
+    '.': 'dot',
+    "-": 'dash'
+}
 
 function convertToMorse(string) {
     console.log(string);
@@ -45,9 +68,9 @@ function convertToMorse(string) {
 
     string.toUpperCase().split('').map(char => {
         if (char == ' ') {
-            morseText += "/";
+            morseText += "/ ";
         } else {
-            
+
             morseText += morse[char] + " ";
             console.log(morseText);
         }
@@ -59,9 +82,68 @@ function convertToMorse(string) {
 
 function copyMorse(morseString) {
     let dialog = document.querySelector('#copyDialog');
-    navigator.clipboard.writeText(morseString);
-
-    if (navigator.clipboard.read() == morseString) {
-        alert('copied');
+    if (navigator.clipboard.writeText(morseString)) {
+        console.log('copied');
+        dialog.focus()
     }
+
 }
+
+function playMorse(morse) {
+    console.log('morse is', morse);
+
+    // const audioEl = document.querySelector('audio');
+    const audioContext = new AudioContext();
+    const dotDuration = 50;
+    const dashDuration = 70;
+    const spaceDuration = 5000;
+
+    const frequency = 2000;
+
+    morse.split('').forEach((code, index) => {
+        setInterval(() => {
+        if (code === ' ') {
+                console.log('space');
+            // }, spaceDuration);
+        }
+        else if (code === '.') {
+            // setTimeout(() => {
+                playCode(audioContext, dotDuration, frequency);
+                console.log('code ', code);
+
+            // }, spaceDuration);
+        } else if (code === '-') {
+            // setTimeout(() => {
+                playCode(audioContext, dashDuration, frequency);
+                console.log('code ', code);
+
+            }
+        }, index * spaceDuration);
+    });
+}
+function playCode(audioContext, duration, frequency) {
+    const oscillator = audioContext.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.value = frequency;
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
+    setTimeout(() => {
+        oscillator.stop();
+    }, 10 * duration);
+    console.log('frequency ', frequency);
+}
+// function playDot(audioContext, duration) {
+//     setTimeout(() => {
+//         audioContext.currentTime = 0;
+//         audioContext.play();
+//         console.log('played dot');
+//     }, duration);
+// }
+
+// function playDash(audioContext, duration) {
+//     setTimeout(() => {
+//         audioContext.currentTime = 0;
+//         audioContext.play();
+//         console.log('played dash');
+//     }, duration);
+// }
